b22d602c2cadc12292df0562e41f2807c69dc43e
Allen Liu <85519235+LSJ-Allen@users.noreply.github.com>
2023-07-23 13:18:38 -0400

    s

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
-app.use(express.json())
 
+app.use(express.json())
+app.use(cors())
 // morgan middleware
 app.use(morgan((tokens, req, res) => {
     const reqType = tokens.method(req, res)
