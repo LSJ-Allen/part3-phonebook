0fd101d01bae0e009eb5873678e4aece2b2b7dc9
Allen Liu <85519235+LSJ-Allen@users.noreply.github.com>
2023-07-26 22:00:46 -0400

    x

--- index.js
+++ index.js
@@ -40,31 +40,11 @@ const errorHandler = (error, req, res, next) => {
     if(error.name === 'CastError'){
         return res.status(400).send({error: "malformatted id"})
     }
+    else if(error.name === 'ValidationError'){
+        return res.status(400).json({error: error.message})
+    }
     next(error)
 }
-
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
 
 app.get('/api/persons', (req, res) => {
     Person.find({}).then(result => {
@@ -116,7 +96,7 @@ const generateId = () => {
     return maxId+1
 }
 
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
     const body = req.body
     // console.log(req.body);
     const name = body.name
@@ -134,6 +114,7 @@ app.post('/api/persons', (req, res) => {
     person.save().then(saved => {
         res.json(saved)
     })
+        .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (req, res, next) => {
